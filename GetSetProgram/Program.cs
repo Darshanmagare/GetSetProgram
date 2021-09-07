d67d7319cae5f8f901d6bea53a4fd9f8023c7411
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 09:35:32 +0530

    Add project files.

---
+++ GetSetProgram/Program.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+
+namespace GetSetProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Getset obj = new Getset();
+            obj.Fname = "Darshan";
+            obj.Lname = "Magare";
+            Console.WriteLine(obj.Fname);
+            Console.WriteLine(obj.Lname);
+
+            //AbstractChildClass obj1 = new AbstractChildClass();
+            //Console.WriteLine(obj1.city);
+
+        }
+    }
+
+
+
+class Getset
+{
+    public Getset() 
+    { 
+    
+    }
+    private string fname;  //private variables
+    private string lname;
+    private int id;
+
+    public string Fname //public properties
+    {
+        get  //get method
+        {
+            return fname;
+        }
+        set
+        {
+            fname = value;
+        }
+    }
+    public string Lname;
+    {
+        get;
+        //set;
+
+    }
+
+    public int Id
+    {
+    get { return id; }
+    set { this.id = value; }
+
+    }
+
+    public void test(int id)
+    {
+    this.id = id;
+    Console.WriteLine(id);
+    }
+
+
+    }
+}
+
+public class child : Getset
+{
+    public void Print()
+    {
+        base.Fname = "Darshan";
+        base.Lname = "Magare";
+        base.test(10);
+    }
+}
+
+public abstract class abstraction
+{
+    private string cityname;
+
+}
+
+
+
+
+public class AbstractChildClass : abstraction
+{
+    public override void abstraction()
+    {
+         public override void abstracttest()
+          {
+        Console.WriteLine("Override method");
+
+          }
+         public 
+    }
+}
+}
